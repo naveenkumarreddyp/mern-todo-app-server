708fe97afb30b914b2d6b3d40d9e586a63a726da
naveenkumarreddyp <92851599+naveenkumarreddyp@users.noreply.github.com>
2022-11-20 21:12:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: ["http://localhost:3000/", "https://todo-app-api-hemx.onrender.com"]
+    origin: ["http://localhost:3000/", "https://todo-app-fiwj.onrender.com"]
 }))
 app.use(cors())
 app.use(todoRoutes)
