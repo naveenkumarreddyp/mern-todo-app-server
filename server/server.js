67833a2041e99642bf8b8ee04c1d74b1d468efe4
Naveen <naveenreddyp9580@gmail.com>
2022-11-20 19:55:28 +0530

    urlchange

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: ["http://localhost:3000/", "https://naveen-todo-app.com"]
+    origin: ["http://localhost:3000/", "https://naveen-todo-app.onrender.com"]
 }))
 // app.use(cors())
 app.use(todoRoutes)
